302a8ec269bc3f2518728170a551894f4adb8f23
César Zurita <zuritacruz37@gmail.com>
2022-11-09 13:30:40 +0100

    add iteration 4

--- js/index.js
+++ js/index.js
@@ -23,9 +23,11 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.parentNode;
+  const targetParent = target.parentNode;
+  targetParent.remove();
+
+  return calculateAll()
 }
 
 // ITERATION 5
@@ -38,5 +40,6 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeProductBtn = document.querySelectorAll('.btn-remove');
+  removeProductBtn.forEach(element => element.addEventListener('click', removeProduct));
 });
