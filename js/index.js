5587ecc8e3187d807ff6ff8ba691f3656b1c4705
César Zurita <zuritacruz37@gmail.com>
2022-11-09 15:45:38 +0100

    add iteration 5, new products remove option to be fixed

--- js/index.js
+++ js/index.js
@@ -21,7 +21,6 @@ function calculateAll() {
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget.parentNode;
   const targetParent = target.parentNode;
@@ -31,9 +30,30 @@ function removeProduct(event) {
 }
 
 // ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  const ProductParent = document.querySelector('.product').parentNode;
+  const newProductName = document.querySelector('.product-name input').value;
+  const newProductPrice = document.querySelector('.product-price input').value;
+
+  let newProduct = document.createElement('tr');
+  newProduct.classList.add('product');
+  newProduct.innerHTML = (`<td class="name">
+    <span>${newProductName}</span>
+    </td>
+    <td class="price">$<span>${newProductPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>`
+  );
+
+  document.querySelector('.product-name input').value = '';
+  document.querySelector('.product-price input').value = 0;
+
+  return ProductParent.appendChild(newProduct)
 }
 
 window.addEventListener('load', () => {
@@ -42,4 +62,7 @@ window.addEventListener('load', () => {
 
   const removeProductBtn = document.querySelectorAll('.btn-remove');
   removeProductBtn.forEach(element => element.addEventListener('click', removeProduct));
+
+  const createProductBtn = document.querySelector('#create');
+  createProductBtn.addEventListener('click', createProduct);
 });
